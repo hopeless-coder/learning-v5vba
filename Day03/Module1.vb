0705f0db2f97dca2a211003fb92fb9689814254e
kneerunjun <kneerunjun@outlook.com>
2015-12-16 10:13:23 +0530

    day01 adding bodies and naming convention

--- Day03/Module1.vb
+++ Day03/Module1.vb
@@ -14,12 +14,51 @@
 
         Dim pDocuments As INFITF.Documents
         pDocuments = pApplication.Documents 'getting the documents collection
-
+        Dim nameOfTheDoc = "Part2.CATPart"
         Dim pDocument As INFITF.Document
-        pDocument = pDocuments.Item(1) 'getting the first document
 
-        Console.WriteLine("the name of the document is {0}", pDocument.Name)
+        For index = 1 To pDocuments.Count
+            If pDocuments.Item(index).Name = nameOfTheDoc Then
+                pDocument = pDocuments.Item(index)
+                Exit For
+            End If
+        Next
+        'start working on the document here
 
+        Dim pPartDoc As MECMOD.PartDocument
+        pPartDoc = pDocument 'this is what is called as the downcasting
+
+        Dim pPart As MECMOD.Part 'this is just the handle to the part
+        pPart = pPartDoc.Part 'this is getting the part
+
+        Dim pBodies As MECMOD.Bodies
+        pBodies = pPart.Bodies
+
+        Dim pPartBody As MECMOD.Body
+        pPartBody = pBodies.Item(1)
+
+        pPartBody.Name = "Changed body name"
+
+        'now here i woudl want to add a new body to the part
+        Dim pNewBody As MECMOD.Body
+
+        pNewBody = pPart.Bodies.Add()
+        pNewBody.Name = "New body added"
+
+        'now an exercise for you to help stretch your muscles(finger)
+        'add 10 bodies to part, 
+        'each body being added has a naming convention
+        ' first body has the name : Body_1
+        ' second body has the name : Body_2
+        ' ... so on.. 
+        ' cn you do this ?
+
+        ' for those of you with supernatural capabilities to finish the above already!
+        ' here is another assignment
+        ' insert 10 bodies
+        ' all the 10 have naming convention
+        ' odd bodies have the names : 1_body, 3_body ...
+        ' even bodies have the names : body_2, body_4 ...
 
     End Sub
 
