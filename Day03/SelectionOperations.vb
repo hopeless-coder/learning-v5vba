30f2ffc8047956c337db27a251f5c59ecdd1f405
kneerunjun <kneerunjun@outlook.com>
2015-12-16 11:32:27 +0530

    chaging the color using selection objects

--- Day03/SelectionOperations.vb
+++ Day03/SelectionOperations.vb
@@ -7,10 +7,17 @@
         pSelection.Add(toDelete)
         pSelection.Delete()
         pSelection.Clear()
+        pd.Part.Update()
     End Sub
 
-    Public Sub SelectAndTurnRed(pd As MECMOD.PartDocument, toDelete As INFITF.AnyObject)
+    Public Sub SelectAndTurnRed(pd As MECMOD.PartDocument, toChange As INFITF.AnyObject)
+        Dim pSelection As INFITF.Selection
+        pSelection = pd.Selection
+        pSelection.Clear()
 
+        pSelection.Add(toChange)
+        pSelection.VisProperties.SetRealColor(255, 0, 0, 0)
+        pd.Part.Update()
     End Sub
 
     Public Sub CopyPaste(pd As MECMOD.PartDocument, source As INFITF.AnyObject, target As INFITF.AnyObject)
