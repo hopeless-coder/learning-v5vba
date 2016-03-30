729b37ab638d39b0d8cad96109ac3e30aa7f27da
kneerunjun <kneerunjun@outlook.com>
2016-03-30 13:04:06 +0530

    created code Day01

---
+++ VBSessions/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBSessions")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Infosys Limited")> 
+<Assembly: AssemblyProduct("VBSessions")> 
+<Assembly: AssemblyCopyright("Copyright © Infosys Limited 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3e7ffc60-18b0-4f4d-ba5d-a28b17521218")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
