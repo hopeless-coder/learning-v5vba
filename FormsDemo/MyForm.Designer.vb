b97bb5505b4c0875466c765b324ac9384a4cd5a3
kneerunjun <kneerunjun@outlook.com>
2016-03-31 11:55:33 +0530

    FormsDemo

---
+++ FormsDemo/MyForm.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDemo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.txtBx = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(90, 105)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'txtBx
+        '
+        Me.txtBx.Location = New System.Drawing.Point(77, 61)
+        Me.txtBx.Name = "txtBx"
+        Me.txtBx.Size = New System.Drawing.Size(100, 20)
+        Me.txtBx.TabIndex = 1
+        '
+        'frmDemo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtBx)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "frmDemo"
+        Me.Text = "My Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents txtBx As System.Windows.Forms.TextBox
+
+End Class
---
+++ FormsDemo/MyForm.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmDemo
+
+    Private Sub frmDemo_Click(sender As Object, e As EventArgs) Handles Me.Click
+        MessageBox.Show("Form is clicked")
+    End Sub
+
+
+    Private Sub frmDemo_Load(sender As Object, e As EventArgs) Handles Me.Load
+        btnSubmit.Enabled = False
+    End Sub
+
+    Private Sub txtBx_TextChanged(sender As Object, e As EventArgs) Handles txtBx.TextChanged
+        txtBx.BackColor = Color.RoyalBlue
+        txtBx.ForeColor = Color.Yellow
+        btnSubmit.Enabled = True
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        MessageBox.Show("Button is clicked")
+    End Sub
+End Class
