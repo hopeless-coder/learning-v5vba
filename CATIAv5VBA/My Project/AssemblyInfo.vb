35c2634ac00e99c7b797eb0fba1e940a67a27a65
kneerunjun <kneerunjun@outlook.com>
2016-04-01 08:58:36 +0530

    added project for catia automation

---
+++ CATIAv5VBA/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CATIAv5VBA")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Infosys Limited")> 
+<Assembly: AssemblyProduct("CATIAv5VBA")> 
+<Assembly: AssemblyCopyright("Copyright © Infosys Limited 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ceeeeb2e-cf5e-4532-b878-c1b2d671e02e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
