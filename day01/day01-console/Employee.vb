ab44cf9df09f1026667e43acdb0aead7a0cd0160
kneerunjun <kneerunjun@gmail.com>
2016-09-30 11:58:02 +0530

    Classes and objects

---
+++ day01/day01-console/Employee.vb
@@ -0,0 +1,20 @@
+﻿Public Class Employee
+    Private Empno As Integer
+    Public Property EmpNumber() As Integer
+        Get
+            Return Empno
+        End Get
+        Set(ByVal value As Integer)
+            Empno = value
+        End Set
+    End Property
+    Private Email As String
+    Public Property EmployeeeEmail() As String
+        Get
+            Return Email
+        End Get
+        Set(ByVal value As String)
+            Email = value
+        End Set
+    End Property
+End Class
--- day01/day01-console/objectvars.vb
+++ day01/day01-console/objectvars.vb
@@ -1,36 +1,42 @@
 ﻿Module objectvars
+
     Sub Main()
+
         ' redefine the boudaries , data has some behaviour attached to it
-        Dim pNames As New Collection 'Object type, complex type
-        'Array : is like the native types, limited in the functionality
-        'pNames = New Collection 'instantiation
+        'Dim pNames As New Collection 'Object type, complex type
+        ''Array : is like the native types, limited in the functionality
+        ''pNames = New Collection 'instantiation
 
-        pNames.Add("Mysore")
-        pNames.Add("Bangalore")
-        pNames.Add("Pune")
-        pNames.Add("Chandigarh")
+        'pNames.Add("Mysore")
+        'pNames.Add("Bangalore")
+        'pNames.Add("Pune")
+        'pNames.Add("Chandigarh")
 
-        Dim flag As Boolean
-        flag = True
+        'Dim flag As Boolean
+        'flag = True
 
-        If pNames.Count > 4 Then
-            Console.WriteLine("The collection has more than 4 elements")
+        'If pNames.Count > 4 Then
+        '    Console.WriteLine("The collection has more than 4 elements")
 
-        ElseIf pNames.Count < 4 Then
-            Console.WriteLine("The collection has less than  4 elements")
-        Else
-            Console.WriteLine("The collection has exactly 4 elements")
-            If flag = True Then
-                Console.WriteLine("and the flag value is true as well")
-            End If
-        End If
+        'ElseIf pNames.Count < 4 Then
+        '    Console.WriteLine("The collection has less than  4 elements")
+        'Else
+        '    Console.WriteLine("The collection has exactly 4 elements")
+        '    If flag = True Then
+        '        Console.WriteLine("and the flag value is true as well")
+        '    End If
+        'End If
 
-        For index = 1 To pNames.Count
-            Console.WriteLine(pNames.Item(index))
-        Next
+        'For index = 1 To pNames.Count
+        '    Console.WriteLine(pNames.Item(index))
+        'Next
+        'pNames = Nothing
 
+        Dim emp As Employee
+        emp = New Employee
 
-        pNames = Nothing
+        emp.EmpNumber = 41993
+        Console.WriteLine(emp.EmpNumber)
         Console.ReadLine()
     End Sub
 End Module
