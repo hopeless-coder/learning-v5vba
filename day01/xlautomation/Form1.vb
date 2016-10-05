f573b3020547753763ec6ba9c779c1ef78005afd
kneerunjun <kneerunjun@gmail.com>
2016-10-05 12:21:06 +0530

    Excel automation basics

---
+++ day01/xlautomation/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.Office.Interop.Excel
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim pXLApp As Application
+        pXLApp = CreateObject("Excel.Application")
+        pXLApp.Visible = True
+        Dim pWkBk As Workbook
+        'pWkBk = pXLApp.Workbooks.Add()
+        pWkBk = pXLApp.Workbooks.Open("D:\\test.xlsx")
+        Dim pWkSheet As Worksheet
+        pWkSheet = pWkBk.Worksheets.Item(1)
+
+        Dim pRange As Range
+        pRange = pWkSheet.UsedRange
+
+        MsgBox(pRange.Cells.Item(1, 2).Value)
+        pRange.Value = "Test value from the automation"
+        pWkBk.Save()
+        pXLApp.Quit()
+    End Sub
+End Class
