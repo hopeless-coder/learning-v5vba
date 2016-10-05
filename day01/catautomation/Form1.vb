856bf23169897657b7cd27c0e20060aac16028d4
kneerunjun <kneerunjun@gmail.com>
2016-10-05 10:23:14 +0530

    creating pads and shape fact access

--- day01/catautomation/Form1.vb
+++ day01/catautomation/Form1.vb
@@ -1,38 +1,61 @@
 ﻿Imports INFITF
 Imports MECMOD
+Imports PARTITF
 Public Class Form1
+    Private pApplication As Application
+    Private pDocument As Document
+    Private pPartDocument As PartDocument
+    Private pPart As Part
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim pApplication As Application
+
         pApplication = GetObject(, "CATIA.Application")
         If pApplication Is Nothing Then
             MsgBox("We could not connect to catia appplication")
         End If
-        MsgBox("We have connected to CATIA")
-
-        Dim pDocument As Document
+        ' MsgBox("We have connected to CATIA")
         pDocument = pApplication.Documents.Item(1) 'in the memory &HFF08
-        MsgBox(pDocument.Name)
-
+        'MsgBox(pDocument.Name)
         'pDocument(&HFF08) -->  : Part1.CATPart
-
-        Dim pPartDocument As PartDocument
         pPartDocument = pDocument
-
         'pPartDocument =pDocument
         'pPartDocument(&HFF08) --> Part1.CATPart
-
-        Dim pPart As Part
         pPart = pPartDocument.Part
 
-        Dim pBody As Body
-        pBody = pPart.Bodies.Item(2)
-
-        pBody.Name = "This name has been changed"
-
+        'Dim pBody As Body
+        'pBody = pPart.Bodies.Item(1)
+        'pBody.Name = "This name has been changed"
         'here I would like to add a body onto the part
-        Dim newBody As Body
-        newBody = pPart.Bodies.Add()
-        newBody.Name = "This is the fresh body added"
+        'Dim newBody As Body
+        'newBody = pPart.Bodies.Add()
+        'newBody.Name = "This is the fresh body added"
+        'pPart.Update()
+        'CreateTenBodies()
+        CreatePadFromSketch()
+    End Sub
+
+    Public Sub CreateTenBodies()
+        Dim pThisBody As Body
+        For index = 1 To 10
+            pThisBody = pPart.Bodies.Add()
+            pThisBody.Name = "Body-" + index.ToString()
+        Next
+        pPart.Update()
+    End Sub
+
+    Public Sub CreatePadFromSketch()
+        Dim pPartBody As Body
+        pPartBody = pPart.Bodies.Item(1)
+        'pPartBody = pPart.MainBody 
+        Dim pSketch As Sketch
+        pSketch = pPartBody.Sketches.Item(1)
+
+        Dim pShapeFac As ShapeFactory ' handle to the shape factory 
+        pShapeFac = pPart.ShapeFactory
+        ' this is defining the domain of the shape facotry
+
+
+        Dim pPad As Pad
+        pPad = pShapeFac.AddNewPad(pSketch, 20.0)
 
         pPart.Update()
     End Sub
