c58bc348d21921b1c59a10a20d82b1d87e1bf734
kneerunjun <kneerunjun@gmail.com>
2016-10-28 11:12:58 +0530

    added to master

---
+++ getting-started/getting-started/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+    Dim pApplication As INFITF.Application
+    Dim pDocument As INFITF.Document
+    Dim pPartDoc As MECMOD.PartDocument
+
+    Sub Main()
+        Connect()
+    End Sub
+
+    Sub Connect()
+        On Error Resume Next
+        pApplication = GetObject(, "CATIA.Application")
+        On Error GoTo 0
+        If pApplication Is Nothing Then
+            Console.WriteLine("failed to connect to catia application")
+            Exit Sub
+        End If
+        pDocument = pApplication.Documents.Item(1)
+        pPartDoc = pDocument
+        Console.WriteLine(pPartDoc.Name)
+        Console.ReadLine()
+    End Sub
+
+End Module
